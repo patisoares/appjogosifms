c912462e2a2d8e4e69074794afeb1405b0c5ca09
patisoares <pati_soares75@hotmail.com>
2021-05-06 00:05:57 -0300

    Associações e multiplicidades - muitos para muitos

--- src/main/java/com/ifms/entities/Equipe.java
+++ src/main/java/com/ifms/entities/Equipe.java
@@ -1,6 +1,8 @@
 package com.ifms.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -35,5 +39,18 @@ public class Equipe implements Serializable{
 	@JoinColumn(name = "modalidade_id")
 	private Modalidade modalidade;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_equipe_atleta", 
+		joinColumns = @JoinColumn(name= "equipe_id"),
+		inverseJoinColumns = @JoinColumn(name= "atleta_id"))
+	
+	Set<Atleta> atletas = new HashSet<>(); 
+	// Set - implementa o conceito de conjunto que não aceita repetições
+	// Set - Interface(não tem implementação)
+	// HashSet - implementa o Set
+	//ATENÇÃ
+	// ... não adicionar a lista no construtor
+	// ... não adicionar o Setter, apenas o Getter para lista, pois queremos
+	// ... manipular a lista e não trocar
 
 }
